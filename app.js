bf606ca594d56bdf59b45c7c139c37ba15d316d0
scottyvg <svangilder@gmail.com>
2017-02-02 11:06:31 -0700

    set up for section 2

---
+++ app.js
@@ -0,0 +1,40 @@
+/* SECTION 2 */
+/* Let & Block Scope */
+
+// var name = 'Scotty';
+// console.log(name);
+// let name = 'Scotty';
+// console.log(name);
+// if (true) {
+//   let name = 'Scotty';
+// }
+// console.log(name);
+// let age = 30;
+// if (true) {
+//   let age = 31;
+//   console.log(age);
+// }
+// console.log(age);
+
+/* Constants with "const" */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
+/* Constants with "const" */
