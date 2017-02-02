654a979d8298e491290c615ca0948c121d5feacd
scottyvg <svangilder@gmail.com>
2017-02-02 11:13:39 -0700

    move directory

--- app.js
+++ app.js
@@ -17,8 +17,21 @@
 // console.log(age);
 
 /* Constants with "const" */
+// const AGE = 30;
+// AGE = 29;
+// console.log(age);
+// const AGES = [27, 29, 31];
+// console.log(AGES);
+// AGES.push(25);
+// console.log(AGES);
+// const OBJ = {
+//   age: 27
+// };
+// console.log(OBJ);
+// OBJ.age = 30;
+// console.log(OBJ);
 
-
+/* Hoisting in ES6 */
 
 
 
