f9eb69ef6c1b5fc2c3875f82038fd0e405925f27
scottyvg <svangilder@gmail.com>
2017-02-03 13:33:17 -0700

    switch file structure to practice class constructors

--- Section-3/app.js
+++ Section-3/app.js
@@ -1,6 +1,23 @@
 /* SECTION 3 */
 /* Modules Setup */
-import * as imported from './external.js';
-console.log(imported.keyValue);
-imported.test();
-console.log(imported.keyValue);
+// import * as imported from './external.js';
+// console.log(imported.keyValue);
+// imported.test();
+// console.log(imported.keyValue);
+
+/* Class Basics */
+// function Person() {
+//
+// }
+//
+// var person = new Person();
+// es 6 syntax
+class Person {
+
+}
+
+let person = new Person();
+
+console.log(person);
+
+/*  */
