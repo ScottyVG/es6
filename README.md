8e88b1bf47dc968c4aee66af71b7c6261884eba1
scottyvg <svangilder@gmail.com>
2017-02-07 11:07:29 -0700

    complete section-9 - reflect api

--- README.md
+++ README.md
@@ -540,3 +540,55 @@ More information may be found here:
   }
  }
  ```
+
+
+ ## Reflect API
+ ### Metaprogramming
+ Metaprogramming means that you’re able to change (parts of) the
+ behavior of the underyling language – JavaScript in this case. This of course is a powerful feature as it allows you to influence the way your code is executed. The Reflect API (like Symbols and Proxies) are important additions which help you with Metaprogramming – something that wasn’t
+ really possible in JavaScript before.
+
+ ### What is the Reflect API?
+ The Reflect API could be described as a collection or “central place” which houses all kind of object and function related functions (for creation, property management etc.). Some of the functionalities added on the Reflect object where available before on the Object object. But the goal for the future is, to have on central place to store all those methods – the Reflect Object/ API. Therefore, the Reflect API provides useful methods to create, manipulate and query objects and functions in your JavaScript project.
+
+ ### Object Construction
+ You can easily create a new object using Reflect.construct().
+ ```
+ class Person {
+ }
+ let person = Reflect.construct(Person, []);
+ ```
+
+ The arguments passed are the constructor/ class used to create a new object, arguments passed to this constructor and, optionally as a third argument, another constructor which should be used.
+
+ Learn more about constructing an object with Reflect here: https://developer.mozilla.org/enUS/docs/Web/JavaScript/Reference/Global_Objects/Reflect/construct
+
+ ### Prototypes
+ You may also use Reflect to set the prototype of an object. Simply do this by using the `setPrototype()` method.
+
+ ```
+ class Person {
+ }
+ let config = {
+  greet() {
+  console.log('Hello there!');
+  }
+ }
+ let person = Reflect.construct(Person, []);
+ Reflect.setPrototypeOf(person, config)
+ ```
+
+ ### Properties
+ Reflect also offers methods to define properties on objects (defineProperty()), delete them (deleteProperty()), as well as get (get()) and set (set()) properties.
+
+ ### Functions
+ You may also use Reflect to execute functions:
+
+ `Reflect.apply(person.greet, person, []);`
+
+ The arguments passed are the function to be executed, what this should be referring to and possible function arguments.
+
+ Learn More
+
+ Find more information on the Reflect API here:
+ https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Reflect
---
+++ Section-9/app.js
@@ -0,0 +1,202 @@
+/* SECTION 8 */
+/* Reflect API */
+
+/* Creating Objects with Reflect.construct() */
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+//
+// function TopObj() {
+//   this.age = 27;
+// }
+//
+// let person = Reflect.construct(Person, ['Scotty'], TopObj);
+// console.log(person);
+// console.log(person instanceof Person);
+// console.log(person.__proto__ == Person.prototype);
+// console.log(person.__proto__ == TopObj.prototype);
+
+/* Calling Functions with Reflect.apply() */
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//
+//   greet(prefix) {
+//     console.log(prefix + 'Hello, I am ' + this.name);
+//   }
+// }
+//
+// let person = Reflect.construct(Person, ['Scotty', 31]);
+// Reflect.apply(person.greet, person, [])
+// Reflect.apply(person.greet, { name: 'Brightin' }, ['...'])
+
+/* Reflect and Prototypes */
+// class Person {
+//   constructor() {
+//     this.name = 'Scotty';
+//   }
+// }
+//
+// let person = new Person();
+// // console.log(Reflect.getPrototypeOf(person));
+// // console.log(person.__proto__ == Person.prototype);
+// // console.log(Reflect.getPrototypeOf(person) == Person.prototype);
+// Person.prototype.age = 27;
+//
+// let proto = {
+//   age: 30
+// };
+//
+// Reflect.setPrototypeOf(person, proto);
+//
+// console.log(Reflect.getPrototypeOf(person));
+
+/* Reflect.construct(), apply() and Prototypes Interaction */
+// class Person {
+//   constructor() {
+//     this.name = 'Scotty';
+//   }
+// }
+//
+// let person = new Person();
+//
+// Person.prototype.age = 27;
+//
+// let proto = {
+//   age: 30,
+//   greet() {
+//     console.log('Hello');
+//   }
+// };
+//
+// Reflect.setPrototypeOf(person, proto);
+//
+// Reflect.apply(person.greet, null, []);
+
+
+/* Accessing Properties with Reflect */
+// class Person {
+//   constructor(name, age) {
+//     this._name = name;
+//     this.age = age;
+//   }
+//
+//   get name() {
+//     return this._name;
+//   }
+//
+//   set name(value) {
+//     this._name = value
+//   }
+// }
+//
+// let mum = {
+//   _name: 'Mum'
+// };
+//
+// let person = new Person('Scotty', 31);
+//
+// // Reflect.set(person, 'name', 'Brightin', mum);
+// //
+// // console.log(mum);
+//
+// // console.log(Reflect.get(person, 'name', mum));
+// console.log(Reflect.has(person, 'name'));
+
+/* Analyzing Objects with Reflect.ownKeys() */
+// class Person {
+//   constructor(name, age) {
+//     this._name = name;
+//     this.age = age;
+//   }
+//
+//   get name() {
+//     return this._name;
+//   }
+//
+//   set name(value) {
+//     this._name = value
+//   }
+// }
+//
+// let mum = {
+//   _name: 'Mum'
+// };
+//
+// let person = new Person('Scotty', 31);
+//
+// console.log(Reflect.ownKeys(person));
+
+/* Creating & Deleting Properties with Reflect */
+// class Person {
+//   constructor(name, age) {
+//     this._name = name;
+//     this.age = age;
+//   }
+//
+//   get name() {
+//     return this._name;
+//   }
+//
+//   set name(value) {
+//     this._name = value
+//   }
+// }
+//
+// let mum = {
+//   _name: 'Mum'
+// };
+//
+// let person = new Person('Scotty', 31);
+//
+// Reflect.defineProperty(person, 'hobbies', {
+//   writable: true,
+//   value: ['Sports', 'Cooking'],
+//
+// })
+//
+// person.hobbies = ['Nothing'];
+//
+// // delete person.age
+// Reflect.deleteProperty(person, 'age');
+//
+// console.log(person.age);
+
+/* Preventing Object Extensions & Wrap Up */
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this.age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value
+  }
+}
+
+let mum = {
+  _name: 'Mum'
+};
+
+let person = new Person('Scotty', 31);
+
+console.log(Reflect.isExtensible(person));
+
+Reflect.preventExtensions(person);
+
+console.log(Reflect.isExtensible(person));
+
+Reflect.defineProperty(person, 'hobbies', {
+  writable: true,
+  value: ['Sports', 'Cooking'],
+})
+
+console.log(person.hobbies);
